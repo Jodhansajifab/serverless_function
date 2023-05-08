18115e22d54b5b798cbbcf85bddfaeb9687b7612
Jodhan Saji <115387369+Jodhansajifab@users.noreply.github.com>
2023-05-08 11:52:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports.handler = async (event) => {
+  const { firstName, secondName } = JSON.parse(event.body);
+
+  const fullName = `${firstName} ${secondName}`;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ fullName }),
+  };
+};
